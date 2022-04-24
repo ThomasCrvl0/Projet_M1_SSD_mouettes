49ce83f788a9957933b0a9301479bac4ad5c20bd
Olivier97400 <91317392+Olivier97400@users.noreply.github.com>
2022-04-24 21:30:28 +0200

    Add files via upload
    Commencement du nouveau script pour les simulations. Dans ce script, je vais améliorer les fonctions afin de les rendre plus automatiques comme avait demandé Mme Brunel.

---
+++ code/script_algo_em.R
@@ -0,0 +1,29 @@
+bird_names = c("European Goldfinch", "Common Linnet", "Common Chaffinch",
+               "European Greenfinch", "Eurasian Bullfinch", "Hawfinch",
+               "Stonechat", "European Robin", "Whinchat", "Song Thrush",
+               "Common Blackbird", "Ring Ouzel", "Mistle Thrush")
+
+mean_volume = c(38.0, 60.9, 58.3, 74.5, 45.0, 71.6, 91.0, 68.4, 51.9, 288.9,
+                293.6, 298.6, 266.1)
+
+sd_volume = c(9.1,  20.8, 15.0,  12.2,  3.8,  12.9,  46.5, 29.8, 27.4, 55.9,
+              78.5,  125.1,  56.6)
+
+# Création du dataframe contenant les données sur les 13 nids d'oiseaux
+# Chaque ligne du df correspond à une espèce d'oiseau
+# Chaque espèce est associée à 2 colonnes qui nous renseignent sur
+# le volume moyen des nids associée à cette espèce respectivement
+# l'ecart type des nids associées à cette espèce
+nest_data = data.frame(bird_names, mean_volume, sd_volume)
+
+# On crée le dataframe regroupant les moyennes et les écarts-types théoriques
+# associés aux espèces d'oiseaux choisies par l'utilisateur
+# Les espèces choisies sont stockées dans un vecteur pris en paramètre
+GetRealParam = function(listOfSpecies){
+  v_index = c()
+  for (species in listOfSpecies){
+    index_sp = which(nest_data$bird_names == species)
+    v_index = append(v_index, index_sp)
+  }
+  return(nest_data[v_index,])
+}

c908b42912e2076b09368728da54923da115ea44
ThomasCrvl0 <91314917+ThomasCrvl0@users.noreply.github.com>
2022-05-03 17:22:46 +0200

    Update script_algo_EM.R
    Début création des boxplots, nouveau Monte Carlo et nouveau calcul des erreurs

--- code/script_algo_EM.R
+++ code/script_algo_EM.R
@@ -9,16 +9,16 @@ mean_volume = c(38.0, 60.9, 58.3, 74.5, 45.0, 71.6, 91.0, 68.4, 51.9, 288.9,
 sd_volume = c(9.1,  20.8, 15.0,  12.2,  3.8,  12.9,  46.5, 29.8, 27.4, 55.9,
               78.5,  125.1,  56.6)
 
-# Création du dataframe contenant les données sur les 13 nids d'oiseaux
-# Chaque ligne du df correspond à une espèce d'oiseau
-# Chaque espèce est associée à 2 colonnes qui nous renseignent sur
-# le volume moyen des nids associée à cette espèce respectivement
-# l'ecart type des nids associées à cette espèce
+# CrÃ©ation du dataframe contenant les donnÃ©es sur les 13 nids d'oiseaux
+# Chaque ligne du df correspond Ã  une espÃ¨ce d'oiseau
+# Chaque espÃ¨ce est associÃ©e Ã  2 colonnes qui nous renseignent sur
+# le volume moyen des nids associÃ©e Ã  cette espÃ¨ce respectivement
+# l'ecart type des nids associÃ©es Ã  cette espÃ¨ce
 nest_data = data.frame(bird_names, mean_volume, sd_volume)
 
-# On crée le dataframe regroupant les moyennes et les écarts-types théoriques
-# associés aux espèces d'oiseaux choisies par l'utilisateur
-# Les espèces choisies sont stockées dans un vecteur pris en paramètre
+# On crÃ©e le dataframe regroupant les moyennes et les Ã©carts-types thÃ©oriques
+# associÃ©s aux espÃ¨ces d'oiseaux choisies par l'utilisateur
+# Les espÃ¨ces choisies sont stockÃ©es dans un vecteur pris en paramÃ¨tre
 GetRealParam = function(list_Species){
   v_index = c()
   for (species in list_Species){
@@ -28,14 +28,14 @@ GetRealParam = function(list_Species){
   return(nest_data[v_index,])
 }
 
-# Choix de 2 espèces European Goldfinch et Ring Ouzel afin d'avoir un mélange
+# Choix de 2 espÃ¨ces European Goldfinch et Ring Ouzel afin d'avoir un mÃ©lange
 # de 2 gaussiennes
 data_test_th = data.frame(bird_names = c("European Goldfinch", "Ring Ouzel")
                    ,proportion_alpha = c(0.3,0.7), mean = c(38, 298.6),
                    sd = c(9.1, 125.1))
 
 
-# Dataframe des données d'initialisations choisies par l'utilisateur
+# Dataframe des donnÃ©es d'initialisations choisies par l'utilisateur
 # On a choisi les espece suivantes: "European Goldfinch"
 # et "Ring Ouzel"
 data_test_init = data.frame(bird_names = c("European Goldfinch", "Ring Ouzel"),
@@ -45,7 +45,7 @@ data_test_init = data.frame(bird_names = c("European Goldfinch", "Ring Ouzel"),
 #data_th = GetRealParam
 simulation = function(data_th, n=100){
   X = rep(NA,n) #echantillon
-  J = dim(data_th)[1] #nb de mélange
+  J = dim(data_th)[1] #nb de mÃ©lange
   vect_alpha = data_th[,2]
   vect_mean = data_th[,3]
   vect_sd = data_th[,4]
@@ -72,7 +72,7 @@ simulation = function(data_th, n=100){
   return(X)
 }
 
-# Fonction générant dataframe (vide)
+# Fonction gÃ©nÃ©rant dataframe (vide)
 # contenant les valeurs de P_thetat(j|X = X_i) (etape E de l'algo)
 param_State_E = function(n, J){
   data_stateE = data.frame(matrix(NA, nrow = n, ncol = J))
@@ -91,7 +91,7 @@ algo_EM = function(data_init, X, K){
     vect_alpha = data_init[,2] #de longueur J
     vect_mean = data_init[,3]
     vect_sd = data_init[,4]
-    v = rep(0,n) #vecteur contenant la somme des des numérateurs de P_thetat(j|X = X_i) 
+    v = rep(0,n) #vecteur contenant la somme des des numÃ©rateurs de P_thetat(j|X = X_i) 
                  #pour chaque valeur de l'echantillon 
     
     # Etape E
@@ -109,15 +109,15 @@ algo_EM = function(data_init, X, K){
     for(col in 2:4){ #on met a jour le data_init
       for(ind in 1:J){
         
-        # on met à jour les alpha
+        # on met Ã  jour les alpha
         if(col == 2){
           data_init[,col][ind] = mean(H[,ind])
         }
-        # on met à jour les mu
+        # on met Ã  jour les mu
         if(col == 3){
           data_init[,col][ind] = (sum(X*H[,ind]))/(sum(H[,ind]))
         } 
-        # on met à jour les sigma
+        # on met Ã  jour les sigma
         if(col == 4){
           data_init[,col][ind] = sqrt((sum( (X-rep(data_init[,col-1][ind],n))^2
                                        *H[,ind] ))/sum(H[,ind]))
@@ -133,50 +133,58 @@ data_test_th
 X = simulation(data_test_th,100)
 algo_EM(data_test_init,X,50)
 
-# Fonction calculant la racine carré de l'erreur quadratique moyenne pour
-# les  paramètre à savoir alpha, mu et sigma
-# les RMSE respectives de ces 3 paramatres sont stockées dans un df
-calcul_rmse = function(df_th, new_df){
-  df_rmse = data.frame(rmse_alpha = NA, rmse_mu = NA, rmse_sigma = NA)
-  df_th[[1]] = NULL
-  for(i in 1:3){
-    df_rmse[[i]] = sqrt(mean((df_th[[i]] - new_df[[i]])^2))
+
+# Calculs des erreurs
+calcul_error = function(data_th, data_EM){
+  J = dim(data_th)[1]
+  df_error = data.frame(error_alpha = rep(NA, J), error_mu = rep(NA, J),
+                       error_sigma = rep(NA, J))
+  for(c in 2:4){
+    df_error[,c-1] = abs(data_th[,c] - data_EM[,c])
   }
-  return(df_rmse)
+  return(df_error)
 }
 
-monteCarlo = function(df_th, df_init, X, k){
-  df_monteCarlo = data.frame(rmse_alpha = rep(NA, k),
-                             rmse_mu = rep(NA, k),
-                             rmse_sigma = rep(NA, k))
+
+# Monte Carlo
+# rÃ©alisation d'un monte carlo avec k echantillons de taille n
+# avec N iterations de l'algorithme EM
+Monte_Carlo = function(data_th, data_init, k, n, N){
+  J = dim(data_th)[1]
+  df_MonteCarlo = data.frame()
+  iteration = c()
   for(i in 1:k){
-    new_df = algo_EM(df_init, X, i)
-    df_rmse = calcul_rmse(df_th, new_df)
-    v_rmse = as.numeric(df_rmse)
-    df_monteCarlo[i, ] = v_rmse
+    X = simulation(data_th, n)
+    data_EM = algo_EM(data_init, X, N)
+    df_error = calcul_error(data_th, data_EM)
+    df_MonteCarlo = rbind((df_MonteCarlo), df_error)
+    v_iter = rep(paste("itÃ©ration",i,sep="_"), J)
+    iteration = append(iteration, v_iter)
   }
-  return(df_monteCarlo)
+  df_MonteCarlo = cbind(iteration, df_MonteCarlo)
+  return(df_MonteCarlo)
 }
 
-# Choix de 2 espèces European Goldfinch et Ring Ouzel afin d'avoir un mélange
+
+# Choix de 2 espÃ¨ces European Goldfinch et Ring Ouzel afin d'avoir un mÃ©lange
 # de 2 gaussiennes
 df_th2 = data.frame(bird_names = c("European Goldfinch", "Ring Ouzel")
                     ,proportion_alpha = c(0.3,0.7), mean_th = c(38, 298.6),
                     sd_th = c(9.1, 125.1))
 
 
-# Dataframe des données d'initialisations choisies par l'utilisateur
-# On a choisi les espèces suivantes: "European Goldfinch"
+# Dataframe des donnÃ©es d'initialisations choisies par l'utilisateur
+# On a choisi les espÃ¨ces suivantes: "European Goldfinch"
 # et "Ring Ouzel"
 data_init2 = data.frame(alpha_init = c(0.2,0.8), mean_init = c(50, 280),
                         sd_init = c(11, 130))
 
-# teste des fonctions simulation et algo_EM pour ce mélange de 2 gaussiennes
+# teste des fonctions simulation et algo_EM pour ce mÃ©lange de 2 gaussiennes
 X2 = simulation(df_th2, 100)
 print(df_th2)
 algo_EM(data_init2, X2, 30)
 
-# Autre exemple avec 3 espèces
+# Autre exemple avec 3 espÃ¨ces
 df_th3 = data.frame(bird_names = c("Stonechat", "European Goldfinch",
                                    "Common Blackbird")
                     ,proportion_alpha = c(0.3,0.5,0.2),
@@ -189,12 +197,12 @@ data_init3 = data.frame(bird_names = c("Stonechat", "European Goldfinch",
                         mean_init = c(110, 20, 275),
                         sd_init = c(55, 21, 98.6))
 
-# teste des fonctions simulation et algo_EM pour ce mélange de 3 gaussiennes
+# teste des fonctions simulation et algo_EM pour ce mÃ©lange de 3 gaussiennes
 X3 = simulation(df_th3, 100)
 print(df_th3)
 algo_EM(data_init3, X3, 30)
 
-# Autre exemple avec 4 espèces
+# Autre exemple avec 4 espÃ¨ces
 df_th4 = data.frame(bird_names = c("Eurasian Bullfinch", "Common Chaffinch",
                                    "Mistle Thrush", "Ring Ouzel")
                     ,proportion_alpha = c(0.3,0.4,0.2, 0.1),
@@ -207,7 +215,81 @@ data_init4 = data.frame(bird_names = c("Eurasian Bullfinch", "Common Chaffinch",
                         mean_init = c(60, 40, 270, 313),
                         sd_init = c(7, 8.25, 42, 142))
 
-# teste des fonctions simulation et algo_EM pour ce mélange de 4 gaussiennes
+# teste des fonctions simulation et algo_EM pour ce mÃ©lange de 4 gaussiennes
 X4 = simulation(df_th4, 100)
 print(df_th4)
 algo_EM(data_init4, X4, 30)
+
+
+
+
+# Boxplots
+
+vect1 <- seq(1, 298, by = 3) # vecteur 1
+vect2 <- seq(2, 299, by = 3) # vecteur 2
+vect3 <- seq(3, 300, by = 3) # vecteur 3
+
+
+
+# Pour les \alpha
+df_monteCarlo_50 = Monte_Carlo(df_th3, data_init3, 100, 50, 10)
+df_monteCarlo_100 = Monte_Carlo(df_th3, data_init3, 100, 100, 10)
+df_monteCarlo_500 = Monte_Carlo(df_th3, data_init3, 100, 500, 10)
+par(mfrow = c(3,3))
+boxplot(df_monteCarlo_50[vect1, 2]) # \alpha_1
+boxplot(df_monteCarlo_100[vect1, 2]) # \alpha_1
+boxplot(df_monteCarlo_500[vect1, 2]) # \alpha_1
+boxplot(df_monteCarlo_50[vect2, 2]) # \alpha_2
+boxplot(df_monteCarlo_100[vect2, 2]) # \alpha_2
+boxplot(df_monteCarlo_500[vect2, 2]) # \alpha_2
+boxplot(df_monteCarlo_50[vect3, 2]) # \alpha_3
+boxplot(df_monteCarlo_100[vect3, 2]) # \alpha_3
+boxplot(df_monteCarlo_500[vect3, 2]) # \alpha_3
+
+
+
+# Pour les \mu
+df_monteCarlo_50 = Monte_Carlo(df_th3, data_init3, 100, 50, 10)
+df_monteCarlo_100 = Monte_Carlo(df_th3, data_init3, 100, 100, 10)
+df_monteCarlo_500 = Monte_Carlo(df_th3, data_init3, 100, 500, 10)
+par(mfrow = c(3,3))
+boxplot(df_monteCarlo_50[vect1, 3]) # \mu_1
+boxplot(df_monteCarlo_100[vect1, 3]) # \mu_1
+boxplot(df_monteCarlo_500[vect1, 3]) # \mu_1
+boxplot(df_monteCarlo_50[vect2, 3]) # \mu_2
+boxplot(df_monteCarlo_100[vect2, 3]) # \mu_2
+boxplot(df_monteCarlo_500[vect2, 3]) # \mu_2
+boxplot(df_monteCarlo_50[vect3, 3]) # \mu_3
+boxplot(df_monteCarlo_100[vect3, 3]) # \mu_3
+boxplot(df_monteCarlo_500[vect3, 3]) # \mu_3
+
+# Pour les \sigma
+df_monteCarlo_50 = Monte_Carlo(df_th3, data_init3, 100, 50, 10)
+df_monteCarlo_100 = Monte_Carlo(df_th3, data_init3, 100, 100, 10)
+df_monteCarlo_500 = Monte_Carlo(df_th3, data_init3, 100, 500, 10)
+par(mfrow = c(3,3))
+boxplot(df_monteCarlo_50[vect1, 4]) # \sigma_1
+boxplot(df_monteCarlo_100[vect1, 4]) # \sigma_1
+boxplot(df_monteCarlo_500[vect1, 4]) # \sigma_1
+boxplot(df_monteCarlo_50[vect2, 4]) # \sigma_2
+boxplot(df_monteCarlo_100[vect2, 4]) # \sigma_2
+boxplot(df_monteCarlo_500[vect2, 4]) # \sigma_2
+boxplot(df_monteCarlo_50[vect3, 4]) # \sigma_3
+boxplot(df_monteCarlo_100[vect3, 4]) # \sigma_3
+boxplot(df_monteCarlo_500[vect3, 4]) # \sigma_3
+
+
+
+# Pour les \v_j
+df_monteCarlo_50 = Monte_Carlo(data_test_th, data_test_init, 100, 50, 10)
+df_monteCarlo_100 = Monte_Carlo(data_test_th, data_test_init, 100, 100, 10)
+df_monteCarlo_500 = Monte_Carlo(data_test_th, data_test_init, 100, 500, 10)
+par(mfrow = c(2,3))
+boxplot(df_monteCarlo_50[vectp, 4]) # \v_1
+boxplot(df_monteCarlo_100[vectp, 4]) # \v_1
+boxplot(df_monteCarlo_500[vectp, 4]) # \v_1
+boxplot(df_monteCarlo_50[vecti, 4]) # \v_2
+boxplot(df_monteCarlo_100[vecti, 4]) # \v_2
+boxplot(df_monteCarlo_500[vecti, 4]) # \v_2
+
+
